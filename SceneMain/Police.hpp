ecb7486adb30459b45ad9539812bb5cf5368b578
Dani Torramilans <dani.torramilans@gmail.com>
2013-10-15 03:10:43 +0200

    Police class is now just a placeholder. Rewrite pls

--- SceneMain/Police.hpp
+++ SceneMain/Police.hpp
@@ -6,34 +6,15 @@
 class Police : public Npc {
 	public:
 		Police();
-
-		vec2f moveCharacter(float delta);
-
+		vec2f moveCharacter(float deltaTime);
 	private:
-
-		vec2f getNewGoal(vec2f position);
-		void lookAtRandomPlace();
-
 		enum State  {
-			STATE_PATROL_MOVING,
-			STATE_PATROL_WATCHING,
-			STATE_ALERT,
-			STATE_CONFUSE,
-			STATE_CHASING_PLAYER,
-			STATE_PLAYER_LOST
+			CHASE = 0,
+			PATROL,
+			CONFUSION
 		};
 
 		State state;
-		float watchingTime;
-		float watchingTimeFacing;
-		float alertTime;
-		vec2f lastAlertPos;
-
-		std::vector<float> lastPosSawTime;
-		std::vector<vec2f> lastPosSawPlayer;
-		std::vector<vec2f> lastDirSawPlayer;
-		std::vector<bool> knowPlayer;
-		int chasingPlayerNum;
 };
 
 #endif // POLICE_H
